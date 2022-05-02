05ff39c03aec7b10d5785ae9031798688176611f
LuanaJara <luanadoj8@gmailcom>
2022-05-02 19:37:10 -0300

    luana

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var fname string
+	fmt.Print("Ingresar nombre: ")
+	fmt.Scan(&fname)
+	fmt.Print("Ingresar direccion : ")
+	var lname string
+	fmt.Scan(&lname)
+	fmt.Print("tus datos son : nombre:  " + fname + " Direccion: " + lname + ".")
+}
