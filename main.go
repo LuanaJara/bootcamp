9df5879422541e1e7187d4510c42a57bc4b316e4
LuanaJara <luanadoj8@gmailcom>
2022-05-08 17:05:52 -0300

    hice un strusct

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 )
+type persona struct{
+	nombre string
+	edad int
+}
 
 func main() {
+fmt.Println(persona {"Luana", 18})
+fmt.Println(persona {"Santi", 19})
+fmt.Println(persona {"Ori", 21})
 
-	var fname string
-	fmt.Print("name: ")
-	fmt.Scan(&fname)
-	fmt.Print("city: ")
-	var lname string
-	fmt.Scan(&lname)
-	fmt.Print("data: name:  " + fname + " city: " + lname + ".")
+s := persona{nombre: "Abuelo", edad: 50}
+fmt.Println(s.nombre)
+
+sp := &s
+fmt.Println(sp.edad)
+
+
 }
