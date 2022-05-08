9aac2f3a9180a2d2f95fd78303b86b65006862b2
LuanaJara <luanadoj8@gmailcom>
2022-05-08 17:52:37 -0300

    if

--- main.go
+++ main.go
@@ -1,28 +1,66 @@
 package main
 
-import 
-	"fmt"
+import "fmt"
 
-	func main() {
-
-		var a [10]int
-     fmt.Println("comienzo:", a)
-
-	 a[9] = 100
-     fmt.Println("dsd:", a)
-     fmt.Println("hst:", a[9])
- 
-	 fmt.Println("cnt:", len(a))
-
-	 b := [10]int{1, 2, 3, 4, 5,6,7,8,9,10}
-     fmt.Println("imp:", b)
-	 
-	 var twoD [5][8]int
-     for i := 0; i < 5; i++ {
-        for j := 0; j < 8; j++ {
-            twoD[i][j] = i + j
-        }
-     }
-     fmt.Println("intrv: ", twoD)
+type Persona struct {
+	nombre string
+	edad   int
 }
-	
+
+func (per *Persona) cargar() {
+	fmt.Print("Ingrese el nombre:")
+	fmt.Scan(&per.nombre)
+	fmt.Print("Ingrese la edad:")
+	fmt.Scan(&per.edad)
+}
+
+func (per Persona) imprimir() {
+	fmt.Println("Datos Personales")
+	fmt.Println("***** **********")
+	fmt.Println("Nombre:", per.nombre)
+	fmt.Println("Edad:", per.edad)
+}
+
+func (per Persona) mayorEdad() {
+	if per.edad >= 18 {
+		fmt.Println(per.nombre, "es mayor de edad")
+	} else {
+		fmt.Println(per.nombre, "no es mayor de edad")
+	}
+}
+
+type Empleado struct {
+	Persona
+	sueldo int
+}
+
+func (emp *Empleado) cargar() {
+	fmt.Println("Ingreso de datos del empleado")
+	emp.Persona.cargar()
+	fmt.Print("Ingrese el sueldo:")
+	fmt.Scan(&emp.sueldo)
+}
+
+func (emp Empleado) imprimir() {
+	emp.Persona.imprimir()
+	fmt.Println("Sueldo:", emp.sueldo)
+}
+
+func (emp Empleado) pagaImpuestos() {
+	if emp.sueldo >= 300 {
+		fmt.Println(emp.nombre, "debe pagar impuestos")
+	} else {
+		fmt.Println(emp.nombre, "no debe pagar impuestos")
+	}
+}
+
+func main() {
+	var persona1 Persona
+	persona1.cargar()
+	persona1.imprimir()
+	persona1.mayorEdad()
+	var empleado1 Empleado
+	empleado1.cargar()
+	empleado1.imprimir()
+	empleado1.pagaImpuestos()
+}
