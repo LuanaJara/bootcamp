0347667ded4f65dc0edebbbef87b2e8d57b45b64
LuanaJara <luanadoj8@gmailcom>
2022-05-08 17:41:33 -0300

    probe el array

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
-import (
+import 
 	"fmt"
-)
-type persona struct{
-	nombre string
-	edad int
+
+	func main() {
+
+		var a [10]int
+     fmt.Println("comienzo:", a)
+
+	 a[9] = 100
+     fmt.Println("dsd:", a)
+     fmt.Println("hst:", a[9])
+ 
+	 fmt.Println("cnt:", len(a))
+
+	 b := [10]int{1, 2, 3, 4, 5,6,7,8,9,10}
+     fmt.Println("imp:", b)
+	 
+	 var twoD [5][8]int
+     for i := 0; i < 5; i++ {
+        for j := 0; j < 8; j++ {
+            twoD[i][j] = i + j
+        }
+     }
+     fmt.Println("intrv: ", twoD)
 }
-
-func main() {
-fmt.Println(persona {"Luana", 18})
-fmt.Println(persona {"Santi", 19})
-fmt.Println(persona {"Ori", 21})
-
-s := persona{nombre: "abuela", edad: 50}
-fmt.Println(s.nombre)
-
-sp := &s
-fmt.Println(sp.edad)
-
-
-}
+	
